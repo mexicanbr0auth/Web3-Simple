26dfa220dcb56d05faba32332efe7fc358fb4fbd
Gabriel <mexicanbr@proton.me>
2026-09-04 13:38:40 -0300

    feat: add aditional infos and correct sintaxe

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from web3 import Web3
 
+
 class Wallet:
-    def __init__(self, private_key=None):
-        self.node = "https://eth-sepolia.g.alchemy.com/v2/alch_JucH1ob__oCy8NasW_ypV"
-        self.private_key = private_key
+    def __init__(self):
+        self.node = ""
+        self.private_key = ""
 
     def check_node(self):
         base_url = self.node.split('/v2/')[0]
@@ -12,27 +13,24 @@ class Wallet:
         if response.status_code == 200:
             print("URL/NODE ATIVO")
         else:
-            return print(f"URL/NODE INVALIDO OU OFFLINE  {base_url} ")
+            print(f"URL/NODE INVALIDO OU OFFLINE {base_url}")
+
     def connect(self):
         self.w3 = Web3(Web3.HTTPProvider(self.node))
-        print(f"LOG:  {self.w3}")
         return self.w3
 
     def config(self):
-        call = connect()
-        self.account = w3.e1th.account.from_key(self.private_key)
+        self.connect()
+        self.account = self.w3.eth.account.from_key(self.private_key)
         return self.account
+
     def show_info_loged(self):
-        call = connect()
-        pass 
-       # print(f"WALLET ADDRESS: {}")
-        #terminar maanha kkkkkkkkkk e corrigir erros 
-
-
-
-
+        print(f"WALLET ADDRESS: {self.account.address}")
+        print(f"BALANCE: {self.w3.from_wei(self.w3.eth.get_balance(self.account.address), 'ether'):.2f} ETH")
+        print(f"LATEST BLOCK: {self.w3.eth.block_number}")
 
 
 if __name__ == "__main__":
-    contact = Wallet()
-    contact.check_node()
+    wallet = Wallet()
+    wallet.config()
+    wallet.show_info_loged()
