db3a5ab287538c6bf665a739dd14a67cd3dd5cc6
Gabriel <mexicanbr@proton.me>
2026-09-05 20:00:32 -0300

    nova funçao de enviar token

--- main.py
+++ main.py
@@ -31,9 +31,23 @@ class Wallet:
         print(f"WALLET ADDRESS: {self.account.address}")
         print(f"BALANCE: {self.w3.from_wei(self.w3.eth.get_balance(self.account.address), 'ether'):.2f} ETH")
         print(f"LATEST BLOCK: {self.w3.eth.block_number}")
+    def send_token(self):
+        txn = {
+            'from': self.account.address,
+            'to': input("Wallet To Send: "),
+            'value': self.w3.to_wei(float(input("Ammount, Ex: 0.001: ")),  'ether'),
+            'gas': 21000,
+            'gasPrice': self.w3.eth.gas_price,
+            'nonce': self.w3.eth.get_transaction_count(self.account.address),
+                }
+        sign_txn = self.w3.eth.account.sign_transaction(txn, self.private_key)
+        tx_hash = self.w3.eth.send_raw_transaction(sign_txn.raw_transaction)
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        print(f"Confirmed On Block: {receipt.blockNumber}")
 
 
 if __name__ == "__main__":
     wallet = Wallet()
     wallet.config()
     wallet.show_info_loged()
+    wallet.send_token()
